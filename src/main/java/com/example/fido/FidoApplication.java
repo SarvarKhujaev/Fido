ba269802cb3dbf4d18a0ee7855dc96331e155f43
SarvarKhujaev <uchixaitachi6@gmail.com>
2024-02-23 01:40:11 +0500

    upd: FidoApplication was changed

--- src/main/java/com/example/fido/FidoApplication.java
+++ src/main/java/com/example/fido/FidoApplication.java
@@ -1,5 +1,6 @@
 package com.example.fido;
 
+import com.example.fido.database.RedisDataControl;
 import org.springframework.boot.SpringApplication;
 import com.example.fido.database.PostgreDataControl;
 import com.example.fido.database.ClickHouseDataControl;
@@ -7,9 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FidoApplication {
-    public static void main( String[] args ) {
+    public static void main( final String[] args ) {
         SpringApplication.run( FidoApplication.class, args );
+        ClickHouseDataControl.getInstance().insertValues();
         PostgreDataControl.getInstance();
-        new ClickHouseDataControl().insertValues();
+        RedisDataControl.getInstance();
     }
 }
