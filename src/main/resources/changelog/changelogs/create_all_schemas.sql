67251c5e3c1a2a09ec4c8da9e62ebc4d156578cb
SKhuzhaev <skhuzhaev@beeline.uz>
2024-04-09 11:35:30 +0500

    upd: partitions were added

--- src/main/resources/changelog/changelogs/create_all_schemas.sql
+++ src/main/resources/changelog/changelogs/create_all_schemas.sql
@@ -1,1 +1,2 @@
 CREATE SCHEMA IF NOT EXISTS entities AUTHORIZATION postgres; -- хранит все таблицы ответственные за все сущности
+CREATE SCHEMA IF NOT EXISTS entities_enums AUTHORIZATION postgres; -- хранит все таблицы ответственные за все сущности
